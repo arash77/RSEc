568497cf0a494ca5fb81df1f7266428b596f0a92
arash77 <arash77@users.noreply.github.com>
2025-06-13 10:54:05 +0000

    deploy: e4264e7539c2a3fa4488b2e2c0979e98176a2c32

--- _nuxt/DrjVfG8H.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./CrUnpoRa.js";const r=a("tools",{state:()=>({metadata:null,loading:!1,error:null}),actions:{async fetchMetadata(){this.loading=!0,this.error=null;try{this.metadata=await $fetch("/combined_metadata.json")}catch(t){this.error=t,console.error("Error fetching metadata:",t)}finally{this.loading=!1}}}});export{r as u};
